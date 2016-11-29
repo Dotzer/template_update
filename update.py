e1a053cb47eef969de948c353d1119ebfe5c0a2f
Dotzer <dotza65@gmail.com>
2016-11-29 12:41:09 +1100

    Converting to PEP8

--- update.py
+++ update.py
@@ -1,51 +1,59 @@
 #!/usr/bin/env python
 
 import base64
-
-passfile = open('/home/dotza/python/base/password','r')
-passw = passfile.readlines()
-passw=base64.b64decode(passw[0].rstrip())
-
-#Initializing ZabbixAPI
 from pyzabbix import ZabbixAPI
+
+passfile = open('/home/dotza/python/base/password', 'r')
+passw = passfile.readlines()
+passw = base64.b64decode(passw[0].rstrip())
+# Initializing ZabbixAPI
 zapi = ZabbixAPI("http://monitor.st65.ru")
-zapi.login("i.kim" , passw )
+zapi.login("i.kim", passw)
 print "Connected to Zabbix API Version %s" % zapi.api_version()
 
-#Fileread
-f1 = open('zabip','r')
+# Fileread
+f1 = open('zabip', 'r')
 iplist = f1.readlines()
 f1.close()
 
-#Adding library for every switch model
-templat = {'2800-10':'10222','2800-28':'10136','2900-24':'10137','3000-24':'13145','2850-28':'10330','2910-52':'10221'}
+# Adding library for every switch model
+templat = {'2800-10': '10222',
+           '2800-28': '10136',
+           '2900-24': '10137',
+           '3000-24': '13145',
+           '2850-28': '10330',
+           '2910-52': '10221',
+           '3000-26': '14104'}
 
+# Spliting string into list with 3 strings
 for x in range(1, len(iplist)):
-#spliting string into list with 3 strings
-	ipcomplex = iplist[x].split("\t")
-	print ipcomplex[0]
-	ip = ipcomplex[0].rstrip()
+    ipcomplex = iplist[x].split("\t")
+    print ipcomplex[0]
+    ip = ipcomplex[0].rstrip()
 
-#Assigning template-ids to modelX variables
-	model1 = ipcomplex[1].rstrip()
-	print model1
-	model2 = ipcomplex[2].rstrip()
-	print model2
-
-
-#Write hostid to variable "hid"
-	for h in zapi.hostinterface.get(output="extend", filter={"ip":ip}):
-		hid = h['hostid']
-
-#Clear old template, add new template
-	zapi.host.update(hostid=hid, templates_clear={"templateid":templat[model1]})
-	zapi.host.update(hostid=hid, templates={"templateid":templat[model2]})
+# Assigning template-ids to modelX variables
+    model1 = ipcomplex[1].rstrip()
+    print model1
+    model2 = ipcomplex[2].rstrip()
+    print model2
 
 
-#TEMPLATES:
-#QTECH QSW2900-24T	=	10137
-#QTECH QSW2910-52T	=	10221
-#QTECH QSW2800-10T	=	10222
-#QTECH QSW2800-28T	=	10136
-#QTECH QSW2850-28T	=	10330
-#DLINK DGS3000-24T	=	13145
+# Write hostid to variable "hid"
+    for h in zapi.hostinterface.get(output="extend", filter={"ip": ip}):
+        hid = h['hostid']
+
+# Clear old template, add new template
+    zapi.host.update(hostid=hid,
+                     templates_clear={"templateid": templat[model1]})
+    zapi.host.update(hostid=hid,
+                     templates={"templateid": templat[model2]})
+
+
+# TEMPLATES:
+# QTECH QSW2900-24T    =    10137
+# QTECH QSW2910-52T    =    10221
+# QTECH QSW2800-10T    =    10222
+# QTECH QSW2800-28T    =    10136
+# QTECH QSW2850-28T    =    10330
+# DLINK DGS3000-24T    =    13145
+# DLINK DGS3000-26T    =    14104
